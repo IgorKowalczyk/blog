7d35175d222d50b8b2d8eb8174583a21fd12123d
IgorKowalczyk <majonezexe@gmail.com>
2019-12-22 15:10:18 +0100

    Blog Structure

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function menu() {
+    const menuicon = document.querySelector('.menu-icon');
+    const mainnav = document.querySelector(".main-nav");
+    if(innerWidth <= 426){
+        mainnav.classList.toggle("hidden");
+    }
+    menuicon.addEventListener("click",(e)=>{
+        mainnav.classList.toggle("hidden");
+    });
+}
+
+neby();
