3f1beda70afda59812c1818f3fc452cd3f8a5bef
IgorKowalczyk <majonezexe@gmail.com>
2019-12-22 20:40:10 +0100

    Footer !!!

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ var scrollposition = window.scrollY;
 var logocontainer = document.getElementsByClassName('nav')[0];
 window.addEventListener('scroll', function() {
 scrollposition = window.scrollY;
-if (scrollposition >= 40) {   logocontainer.classList.add('nav-scrolled');
+if (scrollposition >= 40) {
+logocontainer.classList.add('nav-scrolled');
 } else {
 logocontainer.classList.remove('nav-scrolled');
 }
