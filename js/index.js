6f809d0048ade0476a857fd75fb83e898786ae68
IgorKowalczyk <majonezexe@gmail.com>
2020-01-10 13:00:27 +0100

    Css and js min.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,4 @@
-var scrollposition = window.scrollY;
-var logocontainer = document.getElementsByClassName('nav')[0];
-window.addEventListener('scroll', function() {
-scrollposition = window.scrollY;
-if (scrollposition >= 40) {
-logocontainer.classList.add('nav-scrolled');
-} else {
-logocontainer.classList.remove('nav-scrolled');
-}
-});
+/* (c) IGOR KOWALCZYK. All rights reserved. */
 
 const toggletheme = document.querySelector('.theme-switch input[type="checkbox"]');
 const currenttheme = localStorage.getItem('theme');
@@ -28,6 +19,17 @@ localStorage.setItem('theme', 'light');
 }
 }
 toggletheme.addEventListener('change', switchtheme, false);
+
+var scrollposition = window.scrollY;
+var logocontainer = document.getElementsByClassName('nav')[0];
+window.addEventListener('scroll', function() {
+scrollposition = window.scrollY;
+if (scrollposition >= 40) {
+logocontainer.classList.add('nav-scrolled');
+} else {
+logocontainer.classList.remove('nav-scrolled');
+}
+});
 
 document.addEventListener('click', function(event) {
 if (event.target.tagName !== "A" || !event.target.href) return;
